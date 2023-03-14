172f5deb1c5cdf68dfbfb74cca3c30a216d7d86f
LeoPujol5 <126166365+LeoPujol5@users.noreply.github.com>
2023-03-14 08:39:15 -0300

    Ticmas

---
+++ prueba.js
@@ -0,0 +1,12 @@
+
+
+function mifuncion() {
+    document.body.style.backgroundColor="#E6D463";
+    console.log("VER");
+}
+
+function segundafuncion() {
+   // document.body.style.backgroundColor="burlywood";
+   document.body.style.backgroundColor="#FAEBD7";
+   
+}
